0f36914dc39bfb8ee8ddf95ae5cc9c018321a066
Ji Yim <yimjay@users.noreply.github.com>
2018-05-15 15:26:40 -0700

    initial CLI interface setup

--- lib/twyd/city-scraper.rb
+++ lib/twyd/city-scraper.rb
@@ -1,4 +1,4 @@
-class CityScraper
+class Twyd::CityScraper
   # Scrapes the name and individual websites for the most popular cities in BrigFido's website
   attr_accessor :cities
 
@@ -9,10 +9,13 @@ class CityScraper
   def get_cities
     html = Nokogiri::HTML(open("https://www.bringfido.com/destination/popular/"))
     html.css("div .info h2 a").each do |city|
-      cities[city.text] = {
+      @cities[city.text] = {
         :website => city.attribute("href").value
       }
     end
-    puts cities
+  end
+
+  def list_cities
+    @cities
   end
 end
---
+++ lib/twyd/cli.rb
@@ -0,0 +1,21 @@
+class Twyd::CLI
+
+  def call
+    welcome
+    choose_city
+  end
+
+  def welcome
+    puts " ____  ____   __   _  _  ____  __      _  _  __  ____  _  _    _  _  __   _  _  ____    ____   __    ___ "
+    puts "(_  _)(  _ \ / _\ / )( \(  __)(  )    / )( \(  )(_  _)/ )( \  ( \/ )/  \ / )( \(  _ \  (    \ /  \  / __)"
+    puts "  )(   )   //    \\ \/ / ) _) / (_/\  \ /\ / )(   )(  ) __ (   )  /(  O )) \/ ( )   /   ) D ((  O )( (_ \\"
+    puts " (__) (__\_)\_/\_/ \__/ (____)\____/  (_/\_)(__) (__) \_)(_/  (__/  \__/ \____/(__\_)  (____/ \__/  \___/"
+    puts ""
+    puts "Welcome to TWYD (Travel With Your Dog)! This app was made by someone who loves exploring new cities with her dog, Gretchen, in the hopes that you will keep exploring the world with your dog(s) too."
+    puts ""
+
+  def choose_city
+    Twyd::CityScraper.get_cities
+    puts "Now, tell me the city that you'd like to look up:"
+  end
+end
