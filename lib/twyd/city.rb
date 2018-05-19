f73d5c580de02d10d73faac5c609d1df0b2943e0
Ji Yim <yimjay@users.noreply.github.com>
2018-05-19 16:25:56 -0700

    create find_by_name prototype for city class

--- lib/twyd/city.rb
+++ lib/twyd/city.rb
@@ -1,17 +1,23 @@
 class Twyd::City
   attr_accessor :name, :state, :website
 
-  # # Adds all instances of City
-  # @@all = []
-  #
-  # # Initialize
-  # def initialize
-  #   @@all << self
-  # end
-  #
-  # # Make all array readable
-  # def self.all
-  #   @@all
-  # end
+  # Adds all instances of City
+  @@all = []
+
+  # Initialize
+  def initialize
+    @@all << self
+  end
+
+  # Make all array readable
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.detect do |n|
+      n.name.downcase == name.downcase
+    end
+  end
 
 end
